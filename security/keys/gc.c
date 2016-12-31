50221900112061750b753ac765f2c7536451d127
Harsh Shandilya <msfjarvis@gmail.com>
2016-12-31 17:26:01 +0530

    Merge branch 'LA.BR.1.2.9_rb1.10' of git://codeaurora.org/kernel/msm-3.10 into XOS-7.0
    Change-Id: I64822757d0e6ec08bdca530420b48c306cc8cece

--- security/keys/gc.c
+++ security/keys/gc.c
@@ -201,13 +201,11 @@ static noinline void key_gc_unused_keys(struct list_head *keys)
 		if (test_bit(KEY_FLAG_INSTANTIATED, &key->flags))
 			atomic_dec(&key->user->nikeys);
 
-		/* Throw away the key data if the key is instantiated */
-		if (test_bit(KEY_FLAG_INSTANTIATED, &key->flags) &&
-		    !test_bit(KEY_FLAG_NEGATIVE, &key->flags) &&
-		    key->type->destroy)
+		/* now throw away the key memory */
+		if (key->type->destroy)
 			key->type->destroy(key);
 
-		key_user_put(key->user);
+                key_user_put(key->user);
 
 		kfree(key->description);
 
