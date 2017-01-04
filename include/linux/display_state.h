28b48b0f225b60af1ff32b94b242fb545f390a85
Harshit Jain <Harshit Jain>
2017-01-04 12:26:31 +0530

    jalebi:msm:mdss: Add support for adreno idler to check display_state
    halogenOS edits : Strip "fuckyou" naming
    
    Change-Id: If66615f616c995ca0a39833875465b70c548a5c4
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ include/linux/display_state.h
@@ -0,0 +1,17 @@
+/*
+ * include/linux/display_state.h
+ *
+ * Copyright (c) 2017 Harshit Jain
+ * harshitjain6751@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _LINUX_DISPLAY_STATE_H
+#define _LINUX_DISPLAY_STATE_H
+
+bool display_state(void);
+
+#endif
