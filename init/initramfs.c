91456ee155719e072a4ee2ba12cd320110eaa0d2
Lokesh Vutla <lokeshvutla@ti.com>
2017-02-27 14:28:12 -0800

    initramfs: finish fput() before accessing any binary from initramfs
    Commit 4a9d4b024a31 ("switch fput to task_work_add") implements a
    schedule_work() for completing fput(), but did not guarantee calling
    __fput() after unpacking initramfs.  Because of this, there is a
    possibility that during boot a driver can see ETXTBSY when it tries to
    load a binary from initramfs as fput() is still pending on that binary.
    
    This patch makes sure that fput() is completed after unpacking initramfs
    and removes the call to flush_delayed_fput() in kernel_init() which
    happens very late after unpacking initramfs.
    
    Change-Id: I729094e3a2838a2f82c780252d43a60c3837bd21
    Link: http://lkml.kernel.org/r/20170201140540.22051-1-lokeshvutla@ti.com
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    Reported-by: Murali Karicheri <m-karicheri2@ti.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Tero Kristo <t-kristo@ti.com>
    Cc: Sekhar Nori <nsekhar@ti.com>
    Cc: Nishanth Menon <nm@ti.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- init/initramfs.c
+++ init/initramfs.c
@@ -18,6 +18,8 @@
 #include <linux/dirent.h>
 #include <linux/syscalls.h>
 #include <linux/utime.h>
+#include <linux/initramfs.h>
+#include <linux/file.h>
 
 static __initdata char *message;
 static void __init error(char *x)
@@ -616,6 +618,7 @@ static int __init populate_rootfs(void)
 			printk(KERN_EMERG "Initramfs unpacking failed: %s\n", err);
 		free_initrd();
 #endif
+		flush_delayed_fput();
 		/*
 		 * Try loading default modules from initramfs.  This gives
 		 * us a chance to load before device_initcalls.
