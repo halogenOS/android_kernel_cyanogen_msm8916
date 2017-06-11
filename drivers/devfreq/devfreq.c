0566903b6013e14be5126952078033282acc7639
Harsh Shandilya <msfjarvis@gmail.com>
2017-06-11 16:49:54 +0530

    Revert "devfreq: Avoid competing with low-priority tasks"
    @Aviraxp says @dev-harsh1998 mismerged this
    
    This reverts commit 46025e74f5034f586c5b5f83c581e3cde4090ccc.

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1109,7 +1109,7 @@ static int __init devfreq_init(void)
 	devfreq_wq =
 	    alloc_workqueue("devfreq_wq",
 			    WQ_HIGHPRI | WQ_UNBOUND | WQ_FREEZABLE |
-			    WQ_UNBOUND | WQ_MEM_RECLAIM, 1);
+			    WQ_MEM_RECLAIM, 0);
 	if (IS_ERR(devfreq_wq)) {
 		class_destroy(devfreq_class);
 		pr_err("%s: couldn't create workqueue\n", __FILE__);
